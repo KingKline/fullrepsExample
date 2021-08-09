a0312ba2ede25c365370557f2e431a6870785b36
KingKline <37712044+KingKline@users.noreply.github.com>
2021-08-08 20:24:23 -0400

    Add files via upload

--- js/program.js
+++ js/program.js
@@ -9,30 +9,69 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  //ev.target.appendChild(document.getElementById(data));
 
   var cln = document.getElementById(data).cloneNode(true);
-  
 
   var par = ev.target.parentNode;
-  console.log(par.id)
-  if (ev.target.children.length == 0 & par.id == "grid") {
-  
+  var par2 = par.parentNode;
+
+  if (par.id == "grid") {
+    if (ev.target.children.length != 0) {
+      ev.target.innerHTML = ''
+    }
     cln.id = uid()
-    console.log(ev.path)
     ev.target.appendChild(cln);
-}
+    ev.target.style.backgroundColor = cln.style.backgroundColor
+  } else if (par2.id = "grid") {
+    par.innerHTML = ''
+    cln.id = uid()
+    par.appendChild(cln);
+    par.style.backgroundColor = cln.style.backgroundColor
+  }
 }
 
 function removeItm(data) {
 
     var par = document.getElementById(data).parentNode;
     if (par.id != "sidebar") {
+
         var el = document.getElementById(data);
+        parBackround = el.parentNode
+        console.log(parBackround)
+        parBackround.style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+
         el.remove();
     }
 }
 
-const uid = function(){
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+function removeItemBoxClick(data) {
+  var par = document.getElementById(data);
+  const myNode = document.getElementById(data);
+
+  par.style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+  myNode.innerHTML = '';
+
 }
+
+
+
+function addRow() {
+  var content = document.getElementById("grid")
+  var text = '<div class="grid-date"><input type="date"></div>'
+
+  for (let i = 0; i < 7; i++) {
+    text += '<div id="' + uid() + '" onclick="removeItemBoxClick(id)" class="grid-item" ondrop="drop(event)" ondragover="allowDrop(event)"></div>';
+  }
+  content.innerHTML = content.innerHTML + text;
+}
+
+
+
+
+const uid = function(){
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+//adds first row
+addRow()
